582a66eea552308d5fc582d928d3fdf30611c7d9
psvm <38030098+psvm@users.noreply.github.com>
2019-01-15 16:18:03 +0300

    four sorting methods

--- src/SortingTest.java
+++ src/SortingTest.java
@@ -3,9 +3,10 @@ import java.util.Arrays;
 public class SortingTest {
     public static void main(String[] args) {
         int[] num = {1, 2, 5,  10, 200, 100, 5, 3};
-        System.out.println( "Shaker Sorting: " + Arrays.toString(SortingMethods.shakerSort(num)));
-        System.out.println( "Bubble Sorting: " + Arrays.toString(SortingMethods.bubbleSort(num)));
-        System.out.println( "Comb Sorting:   " + Arrays.toString(SortingMethods.combSort(num)));
+        System.out.println( "Shaker Sorting:    " + Arrays.toString(SortingMethods.shakerSort(num)));
+        System.out.println( "Bubble Sorting:    " + Arrays.toString(SortingMethods.bubbleSort(num)));
+        System.out.println( "Comb Sorting:      " + Arrays.toString(SortingMethods.combSort(num)));
+        System.out.println( "Selection Sorting: " + Arrays.toString(SortingMethods.selectionSort(num)));
 
 
     }
